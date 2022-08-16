41b430388d4e674b38f8a20407e1df6e7c2eb683
tjdgus3160 <tjdgus3160@gmail.com>
2022-08-16 22:31:42 +0900

    MyMBTI : qna page

--- MyMBTI/js/start.js
+++ MyMBTI/js/start.js
@@ -1,17 +1,69 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
+const answerBox = document.querySelector(".answerBox");
+const qBox = document.querySelector(".qBox");
+const statusBar = document.querySelector(".statusBar");
 
-function begin() {
-  main.style.WebkitAnimation = "fadeOut 1s";
-  main.style.animation = "fadeOut 1s";
+let qIdx = 0;
+const totalQna = 12;
 
-  setTimeout(() => {
-    qna.style.WebkitAnimation = "fadeIn 1s";
-    qna.style.animation = "fadeIn 1s";
+function createQnaAnswer(answers) {
+  answers.forEach(({ answer }) => {
+    const answerBtn = document.createElement("button");
+
+    answerBtn.className = ["answerList", "my-3", "py-3", "mx-auto", "fadeIn"].join(" ");
+    answerBtn.innerHTML = answer;
+    answerBox.appendChild(answerBtn);
+  });
+
+  answerBox.onclick = (e) => {
+    if (!e.target.closest(".answerList")) {
+      return;
+    }
+
+    const children = Array.from(document.querySelectorAll(".answerList"));
+
+    children.forEach((child) => {
+      child.disabled = true;
+      child.style.WebkitAnimation = "fadeOut 0.5s";
+      child.style.animation = "fadeOut 0.5s";
+    });
 
     setTimeout(() => {
-      main.style.display = "none";
-      qna.style.display = "block";
+      children.forEach((child) => (child.style.display = "none"));
+
+      viewQna(++qIdx);
     }, 450);
+  };
+}
+
+function viewQna(qIdx) {
+  const { q, a } = qnaList[qIdx];
+
+  qBox.innerHTML = q;
+
+  createQnaAnswer(a);
+
+  statusBar.style.width = (100 / totalQna) * (qIdx + 1) + "%";
+}
+
+function changeSection(current, next) {
+  current.style.WebkitAnimation = "fadeOut 1s";
+  current.style.animation = "fadeOut 1s";
+
+  setTimeout(() => {
+    next.style.WebkitAnimation = "fadeIn 1s";
+    next.style.animation = "fadeIn 1s";
+    setTimeout(() => {
+      current.style.display = "none";
+      next.style.display = "block";
+    }, 450);
+
+    if (next.id === "qna") {
+      viewQna(qIdx);
+    }
   }, 450);
 }
+
+const goQna = () => changeSection(main, qna);
+const goResult = () => changeSection(qna, result);
